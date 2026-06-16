0052bf8996120a18a966b680a7ab9ea0daf95871
Aleksandra <bogdanova466@yandex.ru>
2026-06-16 15:13:35 +0300

    Исправление задач 1 и 3

--- task_3.py
+++ task_3.py
@@ -2,8 +2,7 @@ class PointsForPlace:
     def __init__(self):
         self.points = 0  # Изначально количество очков равно нулю
 
-    @staticmethod
-    def get_points_for_place(place):
+    def get_points_for_place(self, place):
         if place > 100:
             print('Баллы начисляются только первым 100 участникам')
             return None
@@ -18,8 +17,7 @@ class PointsForMeters:
     def __init__(self):
         self.points = 0  # Изначально количество очков равно нулю
 
-    @staticmethod
-    def get_points_for_meters(meters):
+    def get_points_for_meters(self, meters):
         if meters < 0:
             print('Количество метров не может быть отрицательным')
             return None
@@ -31,16 +29,16 @@ class TotalPoints(PointsForPlace, PointsForMeters):
     def __init__(self):
         super().__init__()  # Инициализация родительского класса
 
-    @staticmethod
-    def get_total_points(meters, place):
-        points_for_place = PointsForPlace.get_points_for_place(place)
-        points_for_meters = PointsForMeters.get_points_for_meters(meters)
+    def get_total_points(self, meters, place):
+        points_for_place = self.get_points_for_place(place)
+        points_for_meters = self.get_points_for_meters(meters)
 
+        # Если какой‑то из методов вернул None, возвращаем 0 как целое число
         if points_for_place is None or points_for_meters is None:
-            return None
+            return 0
 
         total = points_for_place + points_for_meters
-        return total
+        return int(total)  # Приводим результат к целому числу
 
 # Тестирование
 points_for_place = PointsForPlace()
@@ -52,4 +50,5 @@ print(points_for_meters.get_points_for_meters(10))  # 5.0
 total_points = TotalPoints()
 print(total_points.get_points_for_place(10))  # 91
 print(total_points.get_points_for_meters(10))  # 5.0
-print(total_points.get_total_points(100, 10))  # 141.0
+print(total_points.get_total_points(100, 10))  # 141 (целое число)
+print(total_points.get_total_points(-5, 150))  # 0 (ошибки → 0)
